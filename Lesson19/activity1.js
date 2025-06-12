541cb9c6e30bfa643c673507262cea5fa5d57c35
aadhya298 <hope.aashi_02@outlook.com>
2025-06-12 23:07:33 +0530

    Lesson19 commit

---
+++ Lesson19/activity1.js
@@ -0,0 +1,34 @@
+var js="I'm Aadhya"
+//Printing a statement
+console.log("Hi")
+console.log(js)
+
+//Data Types 
+//String
+var str="Welcome"
+//Boolean
+var bool1=true
+var bool2=false
+//Literal
+var lit=20
+
+console.log(str)
+console.log(bool1)
+console.log(bool2)
+console.log(lit)
+
+//Using opreators
+var a=65
+var b=20
+
+var add=a+b
+var sub=a-b
+var mul=a*b
+var div=a/b
+var mod=a%b
+
+console.log(add)
+console.log(sub)
+console.log(mul)
+console.log(div)
+console.log(mod)
