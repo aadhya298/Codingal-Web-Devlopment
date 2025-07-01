e9a27bbb503fcb67bc627b7df0db0a562e20579c
aadhya298 <hope.aashi_02@outlook.com>
2025-07-01 22:45:41 +0530

    Lesson24 commit

---
+++ Lesson24/activity1.js
@@ -0,0 +1,7 @@
+document.getElementById("h-id").innerHTML= "Are you excited??"
+
+console.log(document.body.childNodes[1].textContent)
+
+const a= document.getElementsByClassName("p1-class")
+a[0].innerHTML="What is DOM?"
+a[1].innerHTML="DOM stands for Document Object Model"
