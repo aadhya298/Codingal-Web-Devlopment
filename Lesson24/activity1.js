0eb14f2b5a437ef80af8f99ed3145d9e60747fc7
aadhya298 <hope.aashi_02@outlook.com>
2025-07-01 23:01:16 +0530

    Lesson24 commit

--- Lesson24/activity1.js
+++ Lesson24/activity1.js
@@ -1,7 +1,17 @@
-document.getElementById("h-id").innerHTML= "Are you excited??"
+const e=document.getElementById("h-id")
+e.innerHTML= "Are you excited??"
 
 console.log(document.body.childNodes[1].textContent)
 
 const a= document.getElementsByClassName("p1-class")
 a[0].innerHTML="What is DOM?"
 a[1].innerHTML="DOM stands for Document Object Model"
+
+e.style.backgroundColor="lavender";
+e.style.border="2px solid purple";
+
+a[0].style.backgroundColor="pink";
+a[0].style.border="2px solid crimson";
+
+a[1].style.backgroundColor="lightblue";
+a[1].style.border="2px solid blue";
