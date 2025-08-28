5b3f7154263655367891837182c8ae2ce04ac885
aadhya298 <hope.aashi_02@outlook.com>
2025-08-28 23:15:40 +0530

    lesson41 commit

--- Lesson41/demo.py
+++ Lesson41/demo.py
@@ -13,4 +13,14 @@ print("Bonjour!!") #Outside the indentation of function
 outside= intro(48,9)
 print("Printing outside:", outside) # var will store only the first returned value i.e. Aadhya not z
 
+def reoccur(k):
+    if k==1:
+        print(k)
+        return
+    print(k)
+    print("Calling func with k-1")
+    reoccur(k-1)
+    print("Statement to run after ending reoccursion")
 
+
+reoccur(6)
