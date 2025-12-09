7e7b73670fe448a32b20bab0d04438241890ca0a
aadhya298 <hope.aashi_02@outlook.com>
2025-12-09 22:45:32 +0530

    Lesson66 commit

--- Lesson66/demo.py
+++ Lesson66/demo.py
@@ -10,3 +10,11 @@ while ans>0:
     ans=ans>>1
 print(count)
 
+#Power set
+sets=[1,2,3]
+n=2**len(sets)
+for i in range(0,n):
+    for j in range(0,len(sets)):
+        if (i &(1<<j))>0:
+            print(sets[j] , end=" ")
+    print("")
