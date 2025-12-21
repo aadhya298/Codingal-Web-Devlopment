76a3af2c1aa7be8f8d49078780c8ec53eb1826d9
aadhya298 <hope.aashi_02@outlook.com>
2025-12-21 11:38:49 +0530

    lesson68 commit

---
+++ Lesson68/Sqlactvity.sql
@@ -0,0 +1,24 @@
+--Create the STUDENT table if it does not extat
+--NOT NULL is used for NAME to ensure every student record has a name
+CREATE TABLE IF NOT EXISTS STUDENT (
+    ROLL _NO TEXT PRIMARY KEY,
+    NAME TEXT NOT NULL,
+    ADDRESS TEXT,
+    PHONE TEXT,
+    AGE INTEGER
+);
+--Insert sample data into the STUDENT table
+INSERT INTO STUDENT (ROLL _NO, NAME, ADDRESS, PHONE, AGE) VALUES
+    ('1', 'RAM', 'DELHI', '****',18),
+    ('2', 'RAMESH', 'GURGAON','****', 18),
+    ('3', 'SUJIT', 'ROHTAK', 20),
+    ('4', 'SURESH', 'DELHI','****', 18),
+    ('5', 'AMAN', 'ROHTAK','****', 20),
+    ('6', 'HARSH', 'GURGAON','****', 18);
+
+SELECT * FROM STUDENT;
+SELECT * FROM STUDENT WHERE AGE=18 AND ADDRESS='DELHI';
+SELECT * FROM STUDENT WHERE AGE=18 AND NAME='RAM';
+SELECT * FROM STUDENT WHERE NAME='RAM' OR NAME='SUJIT';
+SELECT * FROM STUDENT WHERE AGE=20 OR NAME='RAM';
+SELECT * FROM STUDENT WHERE AGE=18 AND (NAME='RAM' OR NAME='RAMESH');
